09aeb71129dc268e5629d831cd824f58702cd730
NivaldoFarias <badivia.f@gmail.com>
2024-11-12 16:59:34 -0300

    add `overflow: hidden` to html element

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 		Math.round(window.innerWidth / 5) > 224 ? 224 : Math.round(window.innerWidth / 5);
 
 	return (
-		<div className="min-w-screen sm:grid-rows-16 grid min-h-screen gap-y-10 overflow-hidden bg-black pt-4 max-sm:-ml-6 max-sm:grid-flow-row max-sm:grid-cols-4 min-[410px]:max-sm:grid-cols-5 sm:grid-flow-col lg:gap-x-2">
+		<div className="min-w-screen grid min-h-screen gap-y-10 overflow-hidden bg-black pt-4 max-sm:-ml-6 max-sm:grid-flow-row max-sm:grid-cols-4 min-[410px]:max-sm:grid-cols-5 sm:grid-flow-col sm:grid-rows-16 lg:gap-x-2">
 			{new Array(nOfEyesToScreenWidth).fill(null).map((_, index) => (
 				<Eyes
 					key={index}
